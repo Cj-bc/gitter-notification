f8cd5e2fb5aaf2c66bea1962f56d2760bfb5875a
Cj-bc <cj-bc@y-modify.org>
2019-01-27 17:35:47 +0900

    Add: logging echo

--- gitter-notifier.sh
+++ gitter-notifier.sh
@@ -22,9 +22,13 @@ function _help()
 # @return <string daemon_PID>
 _daemon() {
   local room="$1"
+  echo -n "Daemon: making fifo..."
   mkfifo "$gitter_notifier_fifo_name"
+  echo " ok"
+  echo -n "starting gitter-cli..."
   gitter-cli s --room "$room" > $gitter_notifier_fifo_name &
   daemon_pid="$!"
+  echo " ok"
 }
 # }}}
 
@@ -33,25 +37,43 @@ _daemon() {
 # @param <string daemon_pid>
 function _cleanup()
 {
+  echo "Detect Quit code."
+  echo -n "kill daemon..."
   kill $1
+  echo " ok"
+  echo -n "Remove fifo..."
   rm "$gitter_notifier_fifo_name"
+  echo " ok"
+
+  echo -n "Untrap..."
   trap SIGINT
   trap SIGTERM
+  echo " ok"
+  echo "Done."
 }
 # }}}
 
 
+# main(): main loop to pop up notifications {{{2
 main() {
   # TODO: check whether room is available
+  echo -n "starting daemon..."
   _daemon "$1"
+  echo "daemon PID: $daemon_pid"
+  echo -n "setting traps..."
   trap "_cleanup $daemon_pid" SIGINT
   trap "_cleanup $daemon_pid" SIGTERM
+  echo " ok"
+
+  echo "Start main loop... go ahead"
   local date time user line
   while read date time user line; do
     user="${user//[():]/}";
     terminal-notifier -message "$line" -title "GITTER" -subtitle "$user";
   done < "$gitter_notifier_fifo_name"
 }
+# }}}
+
 case "$1" in
   "-h"|"help"|"--help") _help;;
   *) main $@;;
